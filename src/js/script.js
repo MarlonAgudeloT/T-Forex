73ca7dac0c32a4d6fa1e63c70372e2e859890b8c
marlon <mgagudelot@gmail.com>
2021-10-27 21:42:13 -0500

    Cambios en background dark

--- src/js/script.js
+++ src/js/script.js
@@ -6,7 +6,7 @@ const showMenu = (toggleId, navId) =>{
     // Validate that variables exist
     if(toggle && nav){
         toggle.addEventListener('click', ()=>{
-            // We add the show-menu class to the div tag with the nav__menu class
+            // We add the show-menu class to the div tag with the nav__menu class 
             nav.classList.toggle('show-menu')
         })
     }
